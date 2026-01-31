810fdc2e95f5b9da35dde2a5d04830accf55e5b7
Masaharu Miyata <masa3640@gmail.com>
2026-01-31 21:38:11 +0900

    percent add

--- kanji_test/kanji_test.py
+++ kanji_test/kanji_test.py
@@ -6,12 +6,36 @@ f.close
 
 import random
 
-question, answer = random.choice(data)
-print(f"問題:{question}")
-kotae=input("答えは？")
-if kotae == answer:
-  print("good!!")
-else:
-  print("No 解答は:", answer)
+aa = 0
+bb = 0
+cc = 0
+dd = 0
+while True:
+  question, answer = random.choice(data)
+  print(f"問題:{question}")
+  kotae=input("答えは？(0でおわり)").strip()
+  match kotae:
+    case "0":
+      print(f"今回の問題数は{aa}、正解数は{cc}個、不正解数は{dd}、正答率は{bb:.1f}％でした")
+      break
+    case _ if kotae == answer:
+      print("good!!")
+      cc += 1
+    case _:
+      print("No 解答は:", answer)
+      dd += 1
+  aa += 1
+  bb = cc / aa * 100
+  print(f"今の問題数は{aa}、正解数は{cc}個、不正解数は{dd}、正答率は{bb:.1f}％です")
+    
+  # print(f"問題:{question}")
+  # kotae=input("答えは？(0でおわり)").strip()
+  # if kotae == answer:
+  #   print("good!!")
+  #   cc += 1
+  # else:
+  #   print("No 解答は:", answer)
+  # else:
+
 
 # print(data)
