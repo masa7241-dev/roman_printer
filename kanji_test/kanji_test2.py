b812b114c2e5e6888766b4d7c8137732ef97bc17
Masaharu Miyata <masa3640@gmail.com>
2026-02-05 21:13:24 +0900

    wrong list

--- kanji_test/kanji_test2.py
+++ kanji_test/kanji_test2.py
@@ -6,6 +6,7 @@ with open("kanji.csv", encoding="cp932") as f:
   data = list(reader)[1:]
 
 stats = {"total":0,"correct":0, "wrong": 0}
+wrong_list=[]
 
 def quiz(data, stats):
   while True:
@@ -16,7 +17,7 @@ def quiz(data, stats):
     
     match kotae:
       case "0":
-        show_result(stats)
+        show_final_result(stats)
         accuracy_rate = calc_accuracy(stats)
         match accuracy_rate:
           case _ if accuracy_rate >= 90:
@@ -25,13 +26,18 @@ def quiz(data, stats):
             print("Good job!")
           case _:
             print("Keep going!")
+            print(f"今回間違えた問題は{wrong_list}")
         break
+
       case _ if kotae == answer:
         print("good!!")
         stats["correct"] += 1
+
       case _:
         print("No 解答は:", answer)
         stats["wrong"] += 1
+        wrong_list.append((question, answer))
+
     stats["total"] += 1
     
     show_result(stats)
@@ -44,6 +50,10 @@ def calc_accuracy(stats):
 def show_result(stats):
   accuracy_rate = calc_accuracy(stats)
   print(f'今の問題数は{stats["total"]}、正解数は{stats["correct"]}個、不正解数は{stats["wrong"]}、正答率は{accuracy_rate:.1f}％です')
+  
+def show_final_result(stats):
+  accuracy_rate = calc_accuracy(stats)
+  print(f'今回の問題数は{stats["total"]}、正解数は{stats["correct"]}個、正答率は{accuracy_rate:.1f}％です')
 
 quiz(data,stats)
 
